84485240507dfc469a7d2a6bd7b164182a5866c9
Hangarden <52573001+Hangarden@users.noreply.github.com>
2024-02-04 23:33:13 +0900

    [Silver I] Title: 맥주 축제, Time: 644 ms, Memory: 78136 KB -BaekjoonHub

--- 백준/Silver/17503. 맥주 축제/맥주 축제.py
+++ 백준/Silver/17503. 맥주 축제/맥주 축제.py
@@ -1,29 +1,38 @@
 import sys
 import heapq
+# 간 레벨 올려주기는 약간의 훼이크다
+input = sys.stdin.readline
+# 맥주들의 만족도 총합을 더해도 M보다 작다면 만들 수 없는 조합이다. -1
 
-n, m, k = map(int, sys.stdin.readline().split())
+# 축제 기간(N <= 20만), 만족도(M <=무한대), 맥주 종류(K<= 200000)
 
-# 선호도 순서로 정렬하여 입력
-beers = [list(map(int, input().split())) for _ in range(k)]
-beers = sorted(beers,key=lambda x: (x[1],x[0]))
+N, M, K = map(int, input().split())
 
-preference = 0
-pq = []
+happy = []
+for _ in range(K):
+    # 만족도 도수
+    a, b = map(int, input().split())
+    happy.append((a,b))
 
-# 낮은 도수부터 먹어보면서 N을 만족하는지 확인
-# 만족하지 않으면 -1 출력하고 종료
-for i in beers:
-    preference += i[0]
-    heapq.heappush(pq, i[0])
+beers = sorted(happy, key= lambda x:(x[1],x[0]))
 
-    if len(pq) == n:
-        if preference >= m:
-            answer = i[1]
+n = 0
+drinks = []
+happy = 0
+for beer in beers:
+    # 행복도가 낮은 순 같다면 도수가 낮은 것부터 먼저들어옴
+
+    heapq.heappush(drinks, beer[0])
+    # print(drinks)
+    happy += beer[0]
+    # print(happy)
+
+
+    if len(drinks) >= N:
+        if happy >= M:
+            print(beer[1])
             break
         else:
-            preference -= heapq.heappop(pq)
+            happy -= heapq.heappop(drinks)
 else:
     print(-1)
-    exit()
-
-print(answer)
