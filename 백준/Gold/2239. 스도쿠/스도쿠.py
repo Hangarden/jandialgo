db114a85187e40c58c65ac724a8bfe8969597dd6
Hangarden <52573001+Hangarden@users.noreply.github.com>
2024-02-17 19:01:30 +0900

    [Gold IV] Title: 스도쿠, Time: 3912 ms, Memory: 142016 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.py
+++ 백준/Gold/2239. 스도쿠/스도쿠.py
@@ -1,29 +1,64 @@
-def isValid(board, row, col, num):
-    # 행, 열, 그리고 3x3 서브그리드에서 num의 유효성 검사
-    for x in range(9):
-        if board[row][x] == num: return False
-        if board[x][col] == num: return False
-        if board[3*(row//3) + x//3][3*(col//3) + x%3] == num: return False
-    return True
+# 논리적인 아이디어
 
-def solveSudoku(board):
+# 1. 0 -> 빈칸에 숫자들을 넣응면 된다
+# 숫자는 아래와 같은 규칙을 지키며 넣으면 된다.
+    # 1. 같은 행에 똑같은 숫자가 없으면 된다. [row][x] != num
+    # 2. 같은 열에 똑같은 숫자가 없으면 된다. [x][column] != num
+    # 3. 같은 구역에 똑같은 숫자가 없으면 된다.
+        # 3-1. 같은 구역은 어떻게 산정하나?
+            # 0,0 ~ 2,2
+            # 3,3 ~ 5,5
+            # 6,6 ~ 8,8
+        # 3-2. 숫자가 2라면 구역은 [3*(row//3) + x//3 ] [ 3 * (col//3) + x%3 ] 와 같다
+        # [row][x] != num
+        # [x][column] != num
+        # [3*(row//3) + x//3 ] [ 3 * (col//3) + x%3 ]
+        # 를 만족하는 곳에 숫자를 쌓아간다. 만일 만족하는 구역이 없다면 False이다!
+        # 만족하는 구역이 있다면 True!
+
+
+
+# print(sdoku)
+
+# 숫자를 넣을 수 있는지 확인하는 함수
+
+board = []
+
+for _ in range(9):
+    board.append(list(map(int, input())))
+def is_valid(board, row, column, number):
+
+    for x in range(9):
+        if board[row][x] == number:
+            return False
+        if board[x][column] == number:
+            return False
+        if board[3*(row//3) + x//3][3*(column//3) + x % 3] == number:
+            return False
+
+    return True
+# 스토쿠 숫자 놓기
+def sdoku(board):
+
     for i in range(9):
         for j in range(9):
-            if board[i][j] == '0':  # 빈 칸 찾기
-                for num in '123456789':
-                    if isValid(board, i, j, num):
-                        board[i][j] = num  # 시도
-                        if solveSudoku(board):
-                            return True  # 해결됨
-                        board[i][j] = '0'  # 되돌리기
-                return False  # 해결 불가
-    return True  # 모든 빈 칸이 채워짐
+            if board[i][j] == 0:
+                for num in range(1, 10):
+                    if is_valid(board, i, j, num):
+                        board[i][j] = num
+                        if sdoku(board):
+                            return True
+                        board[i][j] = 0
+                return False
 
-# 스도쿠 보드 입력 받기
-sdoku = [list(input()) for _ in range(9)]
+    return True
 
-if solveSudoku(sdoku):
-    for row in sdoku:
-        print(''.join(row))
+
+if sdoku(board):
+    for row in board:
+        for i in row:
+            print(i, end="")
+        print()
 else:
-    print("No solution")
+    print("No answer")
+
