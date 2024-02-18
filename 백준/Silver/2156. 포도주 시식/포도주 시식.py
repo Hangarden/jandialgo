6220957c84805714f511ff8ed564f3adadd8af4b
Hangarden <52573001+Hangarden@users.noreply.github.com>
2024-02-18 15:26:44 +0900

    [Silver I] Title: 포도주 시식, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -1,14 +1,21 @@
+import sys
+
+input = sys.stdin.readline
 n = int(input())
-wines = [0] + [int(input()) for _ in range(n)]  # 포도주 양 입력 받기, 1부터 인덱싱하기 위해 앞에 0 추가
-dp = [0] * (n+1)  # DP 테이블 초기화
+wines = [0]
 
-# 초기 조건 설정
-dp[1] = wines[1]
-if n > 1:
+for _ in range(n):
+    wines.append(int(input()))
+# print(wines)
+dp = [0] * (n+1)
+
+
+if n >= 3:
+    dp[1] = wines[1]
     dp[2] = wines[1] + wines[2]
+    for i in range(3, n+1):
+        dp[i] = max(dp[i-2] + wines[i], dp[i-1], dp[i-3] + wines[i-1] + wines[i])
+    print(dp[n])
+else:
+    print(sum(wines))
 
-# DP를 이용한 최대 포도주 양 계산
-for i in range(3, n+1):
-    dp[i] = max(dp[i-1], dp[i-2] + wines[i], dp[i-3] + wines[i-1] + wines[i])
-
-print(dp[n])
