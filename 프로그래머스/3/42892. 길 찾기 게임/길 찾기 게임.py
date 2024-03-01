9f4245a32c29592da1eb55dc5d408f5e408fb42a
Hangarden <52573001+Hangarden@users.noreply.github.com>
2024-03-01 21:18:40 +0900

    [level 3] Title: 길 찾기 게임, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.py
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.py
@@ -1,34 +1,38 @@
 import sys
-from collections import namedtuple
-sys.setrecursionlimit(10**5)
 
-Node = namedtuple("Node", ['x','y','id'])
-
+sys.setrecursionlimit(10**6)
 def solution(nodeinfo):
     answer = [[]]
-    nodeinfo = [Node(x,y,id) for id, (x,y) in enumerate(nodeinfo, 1)]
-    # print(nodeinfo, max(nodeinfo, key = lambda x:x[1]))
+    
+    nodeinfo = [[x,y,id] for id, (x,y) in enumerate(nodeinfo, 1)]
+    # print(nodeinfo)
     
     def pre(nodes):
         if len(nodes) < 1:
             return []
         
-        pivot = max(nodes, key = lambda node:node.y)
-        res = [pivot.id]
-        res += pre([node for node in nodes if node.x < pivot.x])
-        res += pre([node for node in nodes if node.x > pivot.x])
-        
+        pivot = max(nodes, key = lambda x:x[1])
+        res = [pivot[2]]
+        res += pre([node for node in nodes if node[0] < pivot[0]])
+        res += pre([node for node in nodes if node[0] > pivot[0]])
         return res
-                    
+    
     def post(nodes):
         if len(nodes) < 1:
             return []
         
-        pivot = max(nodes, key = lambda node:node.y)
+        pivot = max(nodes, key = lambda x:x[1])
         res = []
-        res += post([node for node in nodes if node.x < pivot.x])
-        res += post([node for node in nodes if node.x > pivot.x])
-        res.append(pivot.id)
-        
+        res += post([node for node in nodes if node[0] < pivot[0]])
+        res += post([node for node in nodes if node[0] > pivot[0]])
+        res.append(pivot[2])
         return res
-    return pre(nodeinfo),post(nodeinfo)
+    return pre(nodeinfo), post(nodeinfo)
+    # test_left = [node for node in nodeinfo if node[0] < pivot[0]]
+    # test_right = [node for node in nodeinfo if node[0] > pivot[0]]
+    # print(test_left)
+    # print(pivot)
+    # print(test_right)
+    
+    # def 
+    
