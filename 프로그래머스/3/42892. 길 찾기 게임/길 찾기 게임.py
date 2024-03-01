dce4a00930e3696c51823a986900ee4db114fa86
Hangarden <52573001+Hangarden@users.noreply.github.com>
2024-03-01 21:05:08 +0900

    [level 3] Title: 길 찾기 게임, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.py
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.py
@@ -1,46 +1,34 @@
-import sys; sys.setrecursionlimit(10001)
+import sys
 from collections import namedtuple
-# class Node(object):
-#     def __init__(self, info):
-#         self.num = info[2]
-#         self.pos = info[:2]
-#         self.left = None
-#         self.right = None
-        
-# class Node(object):
-#     def __init__(self, info):
-#         self.num = info[2]
-#         self.pos = info[:2]
-#         self.left = None
-#         self.right = None
-        
-# Node = namedtuple('Node', ['x', 'y', 'id'])
-Node = namedtuple('Node', ['x','y','id'])
+sys.setrecursionlimit(10**5)
+
+Node = namedtuple("Node", ['x','y','id'])
+
 def solution(nodeinfo):
     answer = [[]]
-    nodes = [Node(x,y,idx)for idx,(x,y) in enumerate(nodeinfo, 1)]
-    # print(nodes)
+    nodeinfo = [Node(x,y,id) for id, (x,y) in enumerate(nodeinfo, 1)]
+    # print(nodeinfo, max(nodeinfo, key = lambda x:x[1]))
     
     def pre(nodes):
         if len(nodes) < 1:
-            return [node.id for node in nodes]
+            return []
         
-        pivot = max(nodes, key = lambda nodes :nodes.y)
+        pivot = max(nodes, key = lambda node:node.y)
         res = [pivot.id]
         res += pre([node for node in nodes if node.x < pivot.x])
         res += pre([node for node in nodes if node.x > pivot.x])
         
         return res
-    
+                    
     def post(nodes):
         if len(nodes) < 1:
-            return [node.id for node in nodes]
+            return []
         
-        pivot = max(nodes, key = lambda nodes :nodes.y)
+        pivot = max(nodes, key = lambda node:node.y)
         res = []
         res += post([node for node in nodes if node.x < pivot.x])
         res += post([node for node in nodes if node.x > pivot.x])
         res.append(pivot.id)
+        
         return res
-    
-    return pre(nodes), post(nodes)
+    return pre(nodeinfo),post(nodeinfo)
