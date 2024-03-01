4ba078c42af787c320e87e41d248fa312aeb2109
Hangarden <52573001+Hangarden@users.noreply.github.com>
2024-03-01 22:19:28 +0900

    [level 3] Title: 정수 삼각형, Time: 55.17 ms, Memory: 14.5 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,11 +1,5 @@
-# def ddp(triangle):
-    
-#     for i in range(len(trianlge)):
-
 def solution(triangle):
     answer = 0
-    # triangle = [[id,x] for id, x in enumerate(triangle)]
-    # print(len(triangle[-1]))
     
     for j in range(1, len(triangle)): 
         for i in range(len(triangle[j])):
@@ -15,10 +9,5 @@ def solution(triangle):
                 triangle[j][-1] = triangle[j-1][-1] + triangle[j][-1]
             else:
                 triangle[j][i] = triangle[j][i] + max(triangle[j-1][i-1], triangle[j-1][i])
-    # for i in range(len(triangle)):
-        
-    # dp = [] for _ in range(i) for i
-    # dp = [[] for _ in range(i) for i in range(len(triangle))]
-    # print(dp)
+
     return max(triangle[-1])
-    # return max(lst[-1])
