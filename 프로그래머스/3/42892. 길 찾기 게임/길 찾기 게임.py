ead590ed7636219c7e58335157507685b14359f4
Hangarden <52573001+Hangarden@users.noreply.github.com>
2024-03-03 15:47:01 +0900

    [level 3] Title: 길 찾기 게임, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.py
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.py
@@ -1,38 +1,41 @@
 import sys
 
 sys.setrecursionlimit(10**6)
+
 def solution(nodeinfo):
     answer = [[]]
     
-    nodeinfo = [[x,y,id] for id, (x,y) in enumerate(nodeinfo, 1)]
+    # 1. 인덱스를 포함한 리스트로 만든다.
+    nodeinfo = [(x,y, idx) for idx, (x,y) in enumerate(nodeinfo, 1)]
     # print(nodeinfo)
-    
+    nodeinfo.sort(key = lambda x : x[1], reverse = True)
+    # print(nodeinfo[0][0])
+    # 2. 전위순회, 후위순회를 고려한다. 
+    # 3. 전위순회
     def pre(nodes):
         if len(nodes) < 1:
             return []
         
-        pivot = max(nodes, key = lambda x:x[1])
+        pivot = nodes[0]
         res = [pivot[2]]
         res += pre([node for node in nodes if node[0] < pivot[0]])
         res += pre([node for node in nodes if node[0] > pivot[0]])
+        
         return res
     
     def post(nodes):
         if len(nodes) < 1:
             return []
         
-        pivot = max(nodes, key = lambda x:x[1])
+        pivot = nodes[0]
         res = []
         res += post([node for node in nodes if node[0] < pivot[0]])
         res += post([node for node in nodes if node[0] > pivot[0]])
         res.append(pivot[2])
         return res
+
+
+    # 3-1. 루트 -> 왼 -> 오
+    # 왼쪽 노드들의 특징은 root보다 작은 값들의 집합
+    # 오른쪽 노드들의 특징은 root보다 큰 값들의 집합
     return pre(nodeinfo), post(nodeinfo)
-    # test_left = [node for node in nodeinfo if node[0] < pivot[0]]
-    # test_right = [node for node in nodeinfo if node[0] > pivot[0]]
-    # print(test_left)
-    # print(pivot)
-    # print(test_right)
-    
-    # def 
-    
