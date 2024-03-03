6591aeb01f00d335834e9dc928ab71a671c9d251
Hangarden <52573001+Hangarden@users.noreply.github.com>
2024-03-03 19:33:41 +0900

    [level 3] Title: 양과 늑대, Time: 0.47 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -1,28 +1,28 @@
 answer = []
-def dfs(node, sheep, wolf, possibles, info, trees):
+    
+def dfs(node, sheep, wolf, possibles, info, tree):
+    # 방문한 곳이 양인지 늑대인지 확인
     global answer
     if info[node] == 0:
         sheep += 1
     else:
         wolf += 1
-    
+        
+    #양과 늑대 갯수를 확인하고 늑대갯수가 크거나 같다면 전부 잡아먹힘으로 종료
     if sheep > wolf:
         answer.append(sheep)
     else:
-        return
-        
-    possibles.extend(trees[node])
-    for i in possibles:
-        dfs(i, sheep, wolf, [x for x in possibles if x != i], info,trees)
-def solution(info, edges):
+        return 
+    
+    possibles.extend(tree[node])
+    for x in possibles:
+        dfs(x, sheep, wolf, [i for i in possibles if i != x], info,tree)
 
-    
-    trees = [[] for _ in range(len(info))]
-    
+def solution(info, edges):
+    tree = [[] for _ in range(len(info))]
     for x,y in edges:
-        trees[x].append(y)
-        
-    # print(trees)
-    dfs(0,0,0,[], info,trees)
-    # print(answer)
+        tree[x].append(y)
+    # print(tree)
+    dfs(0,0,0,[],info, tree)
+    # return 0
     return max(answer)
