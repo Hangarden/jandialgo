e2fa5c8dfce994174aa2c642e565102c47e962d0
Hangarden <52573001+Hangarden@users.noreply.github.com>
2024-03-07 22:53:34 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,22 +1,40 @@
 def solution(genres, plays):
     answer = []
-
-    dic1 = {}
-    dic2 = {}
-
-    for i, (g, p) in enumerate(zip(genres, plays)):
-        if g not in dic1:
-            dic1[g] = [(i, p)]
+    genre_set = set(genres)
+    total = [(idx, x,y) for idx,(x,y) in enumerate(zip(genres, plays))]
+    # print(total) 
+    
+    
+    # for x,y in 
+    
+    # print(genre_set)
+    
+    #장르 순위를 결정할 dict
+    total_chart = {}
+    # 장르별 플레이리스트를 결정할 dict
+    genre_rank = {}
+    for idx, genre, plays in total:
+        if genre not in total_chart:
+            total_chart[genre] = plays
         else:
-            dic1[g].append((i, p))
-
-        if g not in dic2:
-            dic2[g] = p
+            total_chart[genre] += plays
+            
+        if genre not in genre_rank:
+            genre_rank[genre] = [(plays, idx)]
         else:
-            dic2[g] += p
-
-    for (k, v) in sorted(dic2.items(), key=lambda x:x[1], reverse=True):
-        for (i, p) in sorted(dic1[k], key=lambda x:x[1], reverse=True)[:2]:
-            answer.append(i)
-
+            genre_rank[genre].append((plays, idx))
+    # print(total_chart)
+    # print(genre_rank)
+                                     
+    # total_chart = sorted(total_chart, reverse = True)
+    
+    total_chart = sorted(total_chart.items(), key=lambda x:x[1],reverse = True)
+#     print(total_chart)
+    
+#     genre_rank = sorted(genre_rank[i[0]], key = lambda x : (-x[0], x[1]))[:2]
+#     print(genre_rank)
+    for i in total_chart:
+        for x in sorted(genre_rank[i[0]], key = lambda x : (-x[0], x[1]))[:2]:
+            answer.append(x[1])
+            
     return answer
