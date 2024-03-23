d9715a67af080d143960aed4352a1bf9a9289af0
Hangarden <52573001+Hangarden@users.noreply.github.com>
2024-03-23 09:14:54 +0900

    [Gold V] Title: 치킨 배달, Time: 632 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -14,37 +14,12 @@ for i in range(N):
             chicken.append((i,j))
         if maps[i][j] == 1:
             homes.append((i,j))
-# print(chicken, home)
-
+            
 chickens = list(combinations(chicken,M))
-# print(len(chicken))
 answer = 0
-
-# for i in home:
-#     print(i)
 max_val = float('inf')
-# for x in chicken:
-#     for y in home:
-#         print(x,y)
-#
-
-# for chi in chicken:
-#     print(chi)
-#     for x in chi:
-#         print(x)
-#         for y in home:
-#             print(y)
-
-# print(len(chicken))
-# print(home)
 total_min_val = float('inf')
 
-# for home in homes:
-#     answer = 0
-#     for chi in chicken:
-#         min_val = float('inf')
-#         for x in chi:
-#             min_val = min(min_val, abs[h])
 for chicken in chickens:
     answer = 0
     for home in homes:
@@ -54,18 +29,5 @@ for chicken in chickens:
         answer += min_val
     # print(answer)
     total_min_val = min(answer, total_min_val)
-#
+
 print(total_min_val)
-
-
-# total_min_val = float('inf')
-# for chi in chicken:
-#     answer =0
-#     for x in chi: # 10
-#         min_val = float('inf')
-#         for y in home: # 6
-#             min_val = min(min_val, abs(x[0] - y[0]) + abs(x[1] - y[1]))
-#         answer += min_val
-#         print(answer)
-#     total_min_val = min(answer, total_min_val)
-# print(total_min_val)
