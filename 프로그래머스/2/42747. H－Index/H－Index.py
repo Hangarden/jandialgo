1ac4db909bb7b4223c9179c140bc803aa925be9e
Hangarden <52573001+Hangarden@users.noreply.github.com>
2024-04-22 21:45:23 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -3,7 +3,7 @@ def solution(citations):
     citations.sort(reverse = True)
     
     for i in range(len(citations)):
-        if citations[i] < (i + 1):
+        if citations[i] <= i:
             return i
             
     
