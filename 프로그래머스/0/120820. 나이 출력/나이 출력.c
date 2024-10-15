9fa874760b1035de51995e428731677556b3e65c
Hangarden <52573001+Hangarden@users.noreply.github.com>
2024-10-15 21:46:10 +0900

    [level 0] Title: 나이 출력, Time: 0.01 ms, Memory: 4.44 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int age) {
+    int answer = 0;
+    int standard = 2022;
+    
+    answer = standard - age + 1;
+    
+    return answer;
+}
